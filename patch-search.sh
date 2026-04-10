d574ca3f621312eb7af7c8469b44184c407d3eb7
kmink3225 <kmink3225@gmail.com>
2026-04-11 07:40:21 +0900

    refactor: listing 검색 필터를 Quarto 네이티브 방식으로 전환
    - docs/blog/index.qmd: filter-ui를 리스트로 명시 (categories, description 추가)
    - _quarto.yml: post-render 주석 처리
    - patch-search.sh 삭제 (네이티브 방식으로 대체)

--- patch-search.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# post-render hook: listing 검색 대상에 categories, description 추가
-sed -i 's/searchColumns: \["listing-date","listing-title","listing-author"\]/searchColumns: ["listing-date","listing-title","listing-author","listing-categories","listing-description"]/' _site/docs/blog/index.html
